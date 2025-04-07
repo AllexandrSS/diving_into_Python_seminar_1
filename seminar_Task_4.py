f42325a9d732b1924a30328d53e462b600ff208c
Alexandr S <allexandr.s@gmail.com>
2025-04-07 23:11:47 +0300

    created Task4

---
+++ seminar_Task_4.py
@@ -0,0 +1,8 @@
+"""
+Нарисовать ёлочку
+"""
+rows = int(input("Введите количество рядов: "))
+for i in range (1, rows, +1):
+    spaces = rows - i
+    stars = 2 * i - 1
+    print(" " * spaces + '*' * stars)
