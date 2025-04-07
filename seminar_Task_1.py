9571e36cf06db90299a8a9f1ff5e93064c2fc970
Alexandr S <allexandr.s@gmail.com>
2025-04-07 16:31:42 +0300

    changes Task1 and Task2

--- seminar_Task_1.py
+++ seminar_Task_1.py
@@ -1,4 +1,12 @@
-print("Hello World")
-
-a = 1
-print(a)
+n = int(input('Введите число: '))
+e = int(input('Введите ещё одно число: '))
+count = 2
+sum1 = 0
+while count <= n:
+    if count % e == 0:
+        count += 2
+        continue
+    sum1 += count
+    count += 2
+    print(sum1)
+print(sum1)
